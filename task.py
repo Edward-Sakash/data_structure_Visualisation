8218476f2c6bd1a5b1ba773e720ede77ced6f13f
Edward-Sakash <edward.sakash@yahoo.com>
2023-06-12 12:12:59 +0200

    comments added

--- task.py
+++ task.py
@@ -1,5 +1,11 @@
+"""# Code challenge for next time.
 
+How to solve an equation numerically in a given interval with a given accuracy.
+Let's say we want to find the solution to f(x) = exp(x)+x 
+ in the interval [-1,0] within accuracy 0.0001.
+  How to do it?"""
 ###################################################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 
