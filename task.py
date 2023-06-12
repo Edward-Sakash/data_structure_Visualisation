6e01f2893033bca68d822ab46d89bcc8be3ad32d
Edward-Sakash <edward.sakash@yahoo.com>
2023-06-12 12:25:23 +0200

    added another one method

--- task.py
+++ task.py
@@ -52,3 +52,52 @@ plt.legend()
 plt.title('Plot of f(x) = exp(x) + x')
 plt.grid(True)
 plt.show()
+
+print("_________________________________________________")
+
+#Solution 2
+# the Fixed-Point Iteration method to solve the equation
+#  f(x) = exp(x) + x within a given interval and accuracy
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return np.exp(x) + x
+
+def fixed_point_iteration(g, x0, accuracy):
+    x = x0
+    while True:
+        x_new = g(x)
+        if abs(x_new - x) < accuracy:
+            break
+        x = x_new
+    return x_new
+
+# Define the equation and the iteration function
+equation = lambda x: f(x) - x
+g = lambda x: -np.log(np.abs(x)) - np.log(1/np.abs(x) + 1) if x < 0 else -np.log(1 + x)
+
+# Set the interval and desired accuracy
+a = -1
+b = 0
+accuracy = 0.0001
+
+# Perform fixed-point iteration to find the solution
+solution = fixed_point_iteration(g, (a + b) / 2, accuracy)
+print("The solution is:", solution)
+
+# Plot the function and the solution
+x = np.linspace(a, b, 100)
+y = f(x)
+
+plt.plot(x, y, label='f(x) = exp(x) + x')
+plt.axhline(y=0, color='black', linestyle='--')
+plt.axvline(x=solution, color='red', linestyle='--', label='Solution')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.legend()
+plt.title('Plot of f(x) = exp(x) + x')
+plt.grid(True)
+plt.show()
+
